ad40d306454dc1aac3e73e334a8d81da66802f6b
Kelechi Alan <68669988+KayceeAlan@users.noreply.github.com>
2020-09-05 15:52:55 +0100

    Add files via upload

---
+++ NetfleexBuild/precache-manifest.f607447fdc64bf7b0e2b4d7ee1f91256.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "712fe8b4efb3185806c004644b119770",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2d2f1c10251abe0e6ac",
+    "url": "/static/css/main.22544ce0.chunk.css"
+  },
+  {
+    "revision": "069f94549ec949da6786",
+    "url": "/static/js/2.0bd53d70.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.0bd53d70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2d2f1c10251abe0e6ac",
+    "url": "/static/js/main.1b4d21e8.chunk.js"
+  },
+  {
+    "revision": "bda5f69ac2744488b0ae",
+    "url": "/static/js/runtime-main.4387da79.js"
+  }
+]);
